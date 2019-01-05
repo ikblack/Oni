6b62533157ec4e52f0003891e2950c39a96ae9ea
ikblack <624375676@qq.com>
2019-01-05 17:17:47 +0800

    17.17

---
+++ Assets/Script/GamePlay/ChoosePlayer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoosePlayer : MonoBehaviour {
+
+    public GameObject[] players;
+    public static ChoosePlayer instance;
+    void Start () {
+        instance = this;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+       
+    }
+
+    public void ShowPlayer(int index)
+    {
+        int j = index % players.Length;
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (i == j)
+            {
+                players[j].SetActive(true);
+                LoadData.instance.SaveIdData(j);
+            }
+            else
+                players[i].SetActive(false);
+        }
+    }
+
+    public void OnGUI()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            ShowPlayer(0);
+        }
+    }
+
+
+}
---
+++ Assets/Script/GamePlay/LoadData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadData : MonoBehaviour {
+
+    public static LoadData instance;
+    public int PlayerID;
+    public int Score;
+    public int Rank;
+
+	void Awake () {
+        instance = this;
+	}
+	
+	
+	void Update () {
+       
+	}
+    public void SaveIdData(int index)
+    {
+        PlayerPrefs.SetInt("Role", index);
+        Debug.Log(index);
+    }
+
+    public void SavePlayerData(int PlayerID, int Score, int Rank)
+    {
+        PlayerPrefs.SetInt("Role", PlayerID);
+        PlayerPrefs.SetInt("Score", Score);
+        PlayerPrefs.SetInt("Rank", Rank);
+    }
+    public void LoadPlayerData(int PlayerID,int Score,int Rank)
+    {
+        this.PlayerID = PlayerPrefs.GetInt("Role");
+        this.Score = PlayerPrefs.GetInt("Score");
+        this.Rank = PlayerPrefs.GetInt("Rank");
+    }
+}
